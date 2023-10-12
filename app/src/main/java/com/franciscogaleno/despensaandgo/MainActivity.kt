88520e3c64f6b3209a1c03dba189525ae6be822e
Francisco <franciscogalenorojas@gmail.com>
2023-10-12 08:36:19 -0300

    add images and final details

--- app/src/main/java/com/franciscogaleno/despensaandgo/MainActivity.kt
+++ app/src/main/java/com/franciscogaleno/despensaandgo/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.franciscogaleno.despensaandgo
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import com.example.despensaandgo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun onAboutButtonClick(view: View?)
+    {
+        val builder: AlertDialog.Builder = androidx.appcompat.app.AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.about))
+        builder.setMessage(getString(R.string.developer))
+        builder.show()
     }
 
     public fun onShoppingListButtonClick(view: View?)
